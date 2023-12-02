b86d3c098b30866747d980e0a9ff069b1675c24a
Vladisvell <vladisvell@gmail.com>
2023-12-02 18:46:50 +0500

    added first files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EgeBot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile("appsettings.json");
+            var config = configurationBuilder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            var tokenString = config.GetConnectionString("BotToken");
+
+            //builder.Services.AddDbContext<BotDbContext>(options => options.UseNpgsql(connectionString));
+            var bot = new Bot(tokenString);
+            bot.Run();
+            
+            using IHost host = builder.Build();
+            host.Run();
+        }
+    }
+}
