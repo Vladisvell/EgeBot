2454f18ce80cc3f022e4539321256dd7d316fad3
Vladisvell <73733747+Vladisvell@users.noreply.github.com>
2024-01-09 18:59:11 +0500

    refactor: Moved attribute files to a higher level (#9)

--- Bot.Services/Attributes/MessageHandler.cs
+++ Bot.Services/Attributes/MessageHandler.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EgeBot.Bot.Services
+namespace EgeBot.Bot.Services.Attributes
 {
     [AttributeUsage(AttributeTargets.Method)]
     public class MessageHandlerAttribute : Attribute
